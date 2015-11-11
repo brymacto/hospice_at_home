65e169f2a8e3b6529d6369f957b828c43e70d99a
Bryan MacDiarmid <bryanm@nulogy.com>
2015-11-11 16:17:01 -0500

    Clients controller spec added

---
+++ spec/controllers/clients_controller_spec.rb
@@ -0,0 +1,9 @@
+describe 'POST #create' do
+  it 'renders the client that was created' do
+    client = create(:client)
+
+    post :create, {:last_name => 'Doe', :first_name => 'John'}
+
+    expect(response).to render_template(:show)
+  end
+end
