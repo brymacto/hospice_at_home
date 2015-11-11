c83c08b638cd722bd55e987c302c7be943249986
Bryan MacDiarmid <bryanm@nulogy.com>
2015-11-11 17:19:04 -0500

    Integration test corrected

--- spec/features/clients_spec.rb
+++ spec/features/clients_spec.rb
@@ -1,8 +1,8 @@
 feature "clients" do
   scenario "add a client" do
-    visit 'new_clients_path'
-    fill_in('First Name', :with => 'John')
-    fill_in('First Name', :with => 'Doe')
+    visit new_client_path
+    fill_in('client_first_name', :with => 'John')
+    fill_in('client_last_name', :with => 'Doe')
     click_button('Submit')
     visit '/clients/1'
     expect(page).to have_content 'John'
