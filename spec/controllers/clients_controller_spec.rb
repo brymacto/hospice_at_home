ea7382746178936f245ac33aa56e0d83c0f1f724
Bryan MacDiarmid <bryanm@nulogy.com>
2015-11-11 17:09:26 -0500

    Controller spec for 'new' added

--- spec/controllers/clients_controller_spec.rb
+++ spec/controllers/clients_controller_spec.rb
@@ -3,12 +3,20 @@ require 'rails_helper'
 
 RSpec.describe ClientsController, type: :controller do
   describe 'POST #create' do
-    fit 'renders the client that was created' do
+    it 'renders the client that was created' do
       client = create(:client)
 
       post :create, :client => {:last_name => 'Doe', :first_name => 'John'}
 
       expect(response).to redirect_to("/clients/#{assigns(:client).id}")
+    end
+  end
+
+  describe 'GET #new' do
+    subject { get :new }
+
+    fit "renders the new view" do
+      expect(subject).to render_template(:new)
     end
   end
 end
