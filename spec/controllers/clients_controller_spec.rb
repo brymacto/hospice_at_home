c4f750f722167bf90f9f69e6d1a75745ba8a57a6
Bryan MacDiarmid <bryanm@nulogy.com>
2015-11-11 16:35:03 -0500

    Clients controller spec updated

--- spec/controllers/clients_controller_spec.rb
+++ spec/controllers/clients_controller_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
-describe 'POST #create' do
-  it 'renders the client that was created' do
-    client = create(:client)
 
-    post :create, {:last_name => 'Doe', :first_name => 'John'}
+RSpec.describe ClientsController, type: :controller do
+  describe 'POST #create' do
+    it 'renders the client that was created' do
+      client = create(:client)
 
-    expect(response).to render_template(:show)
+      post :create, {:last_name => 'Doe', :first_name => 'John'}
+
+      expect(response).to render_template(:show)
+    end
   end
 end
