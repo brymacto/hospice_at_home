882a8a3575c6d4883c31f2c4fbaf16419a67fb35
Bryan MacDiarmid <bryanm@nulogy.com>
2015-11-13 11:49:43 -0500

    Match model specs updated

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,17 +1,21 @@
 describe Match do
+  let!(:test_volunteer) { create(:volunteer) }
+  let!(:test_client) { create(:client) }
+  let!(:test_match) { create(:match, client_id: test_client.id, volunteer_id: test_volunteer.id) }
+
   describe "#name" do
     it "returns the name of the client followed by volunteer" do
-
+      expect(test_match.name).to eq('John Doe and Jane Doe')
     end
   end
   describe "#client" do
     it "returns the name of the client" do
-
+      expect(test_match.name).to eq('John Doe')
     end
   end
   describe "#volunteer" do
     it "returns the name of the volunteer" do
-
+      expect(test_match.name).to eq('Jane Doe')
     end
   end
 
