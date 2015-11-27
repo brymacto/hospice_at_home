1e9290e96720349c3248c759249d0a2d31335900
Bryan MacDiarmid <bryanm@nulogy.com>
2015-11-27 18:06:57 -0500

    Checks whether volunteer is available for given shift ('#available?')

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -5,4 +5,15 @@ class Volunteer < ActiveRecord::Base
   def name
     "#{first_name} #{last_name}"
   end
+
+  def available?(day, start_time, end_time)
+    availabilities = volunteer_availabilities
+    matching_availabilities = volunteer_availabilities.select { |availability| (availability.day == day) && (availability.start_hour >= start_time) && (availability.end_hour <= end_time)}
+    return true if matching_availabilities && matching_availabilities.size > 0
+    return false
+    # availabilities_on_day = volunteer_availabilities.select { |availability| availability.day = day }
+    # availabilities_on_day.each do |availability|
+    #   return true if ((availability.start_hour >= start_time) && (availability.end_hour <= end_time)
+    # end
+  end
 end
--- spec/models/volunteer_spec.rb
+++ spec/models/volunteer_spec.rb
@@ -1,11 +1,22 @@
 describe "Volunteer" do
-let!(:test_volunteer) { FactoryGirl.create(:volunteer) }
-let!(:test_availability) { FactoryGirl.create(:volunteer_availability, day: 'monday', start_hour: 14, end_hour: 16, volunteer_id: test_volunteer.id) }
+let!(:volunteer_with_availability) { FactoryGirl.create(:volunteer) }
+let!(:volunteer_without_availability) { FactoryGirl.create(:volunteer) }
+let!(:test_availability) { FactoryGirl.create(:volunteer_availability, day: 'monday', start_hour: 14, end_hour: 16, volunteer_id: volunteer_with_availability.id) }
   describe "#available?" do
-    it "returns true" do
-      expect(test_volunteer.available?(Time.now)).to eq(true)
+    it "is available when volunteer has matching availability" do
+      expect(volunteer_with_availability.available?('monday', 14, 16)).to eq(true)
+    end
+
+    it "is not available when volunteer does not have matching availability" do
+      expect(volunteer_with_availability.available?('sunday', 10, 11)).to eq(false)
+    end
+
+    it "is not available when volunteer has matching availability for only portion of shift" do
+      expect(volunteer_with_availability.available?('sunday', 10, 15)).to eq(false)
+    end
+
+    it "is not available when volunteer has no availabilities" do
+      expect(volunteer_without_availability.available?('sunday', 10, 15)).to eq(false)
     end
   end
-
-
 end
