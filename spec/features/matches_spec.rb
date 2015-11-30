f8cefb4e1797920b2722a3c33206d76de351e73a
Bryan MacDiarmid <bryanm@nulogy.com>
2015-11-30 16:30:57 -0500

    Adds client tests

--- spec/features/matches_spec.rb
+++ spec/features/matches_spec.rb
@@ -16,16 +16,25 @@ feature 'matches' do
     expect(page).to have_content 'Jane'
   end
 
-  # scenario "view list of clients" do
-  #   visit clients_path
-  #   expect(page).to have_content 'John'
-  # end
-  #
-  # scenario "edit client" do
-  #   visit edit_client_path(test_client.id)
-  #   expect(page).to have_content 'Edit'
-  #   fill_in('client_first_name', :with => 'Jon')
-  #   click_button('Submit')
-  #   expect(page).to have_content 'Jon'
-  # end
+  scenario "view list of clients" do
+    visit clients_path
+    expect(page).to have_content test_client.first_name
+  end
+
+  scenario "edit client" do
+    visit edit_client_path(test_client.id)
+    expect(page).to have_content 'Edit'
+    fill_in('client_first_name', :with => 'Jon')
+    click_button('Submit')
+    expect(page).to have_content 'Jon'
+  end
+
+  scenario "delete client" do
+    visit clients_path
+    expect(page).to have_content test_client.first_name
+    visit edit_client_path(test_client.id)
+    click_link('Delete')
+    visit clients_path
+    expect(page).to have_no_content test_client.first_name
+  end
 end
