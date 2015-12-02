7a3c2b40de39705aa7987fdd632ea013ca4810c3
Bryan MacDiarmid <bryanm@nulogy.com>
2015-12-02 15:29:05 -0500

    Adds volunteers table to match explorer front end

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -23,8 +23,7 @@ class MatchesController < ApplicationController
 
   def explorer
     @day_options = Date::DAYNAMES.zip(Date::DAYNAMES.map(&:downcase))
-    @day_options_names = Date::DAYNAMES
-    @day_options_values = Date::DAYNAMES.map(&:downcase)
+    @volunteers = Volunteer.all.order(id: :desc)
   end
 
   def index
