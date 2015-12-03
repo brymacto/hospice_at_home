906062e6143b65e711eb6c0aba7388a70fba9600
Bryan MacDiarmid <bryanm@nulogy.com>
2015-12-03 10:49:53 -0500

    Adds matches controller spec

---
+++ spec/controllers/matches_controller_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe MatchesController, type: :controller do
+  let!(:test_volunteer_available) do
+    create(
+      :volunteer,
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name)
+  end
+  let!(:test_volunteer_not_available) do
+    create(
+      :volunteer,
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name)
+  end
+  let!(:test_volunteer_availability) do
+    create(:volunteer_availability,
+           volunteer_id: test_volunteer_available.id,
+           day: 'monday',
+           start_hour: 10,
+           end_hour: 24)
+  end
+
+  describe 'GET #explorer' do
+    it 'assigns all volunteers when no search params are included' do
+      get :explorer
+
+      expect(assigns(:volunteers)).to include(test_volunteer_available)
+      expect(assigns(:volunteers)).to include(test_volunteer_not_available)
+    end
+
+    it 'assigns @volunteers correctly based on search params' do
+      get :explorer, day: 'monday', start_time: 10, end_time: 12
+
+      expect(assigns(:volunteers)).to include(test_volunteer_available)
+      expect(assigns(:volunteers)).to_not include(test_volunteer_not_available)
+    end
+
+    it 'assigns empty @volunteers when none match search params' do
+      get :explorer, day: 'tuesday', start_time: 10, end_time: 12
+
+      expect(assigns(:volunteers)).to be_empty
+    end
+  end
+end
