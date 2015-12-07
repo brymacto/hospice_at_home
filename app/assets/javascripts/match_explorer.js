3d52747da1fb3072cf7571b8f774e01913477ad5
Bryan MacDiarmid <bryanm@nulogy.com>
2015-12-07 17:21:14 -0500

    Adds functionality to 'select for match' link - now replaces the form field value.

--- app/assets/javascripts/match_explorer.js
+++ app/assets/javascripts/match_explorer.js
@@ -1,6 +1,9 @@
-$(document).on('ready page:load', function() {
-        $( ".select_for_match" ).click(function (event) {
-            event.preventDefault();
-
-        });
+$(document).on('ready page:load', function () {
+    $(".select_for_match").click(function (event) {
+        volunteer_id = this.dataset.volunteerId
+        event.preventDefault();
+        $("select#match_volunteer_id option").filter(function () {
+            return $(this).val() == volunteer_id;
+        }).prop('selected', true);
     });
+});
