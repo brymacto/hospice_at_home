dbc5818df70c8d959ddeb1375926d0620b1f61c3
Jason CheongKeeYou <jasonc@nulogy.com>
2015-12-11 17:05:42 -0500

    Extracts service class from controller.

--- app/controllers/match_proposals_controller.rb
+++ app/controllers/match_proposals_controller.rb
@@ -1,34 +1,61 @@
+class CreateMatchProposal
+  def initialize(params)
+    @params = params
+    @match_proposal = create_match_proposal
+  end
+
+  def successful?
+    @match_proposal.errors.none?
+  end
+
+  def match_proposal
+    @match_proposal
+  end
+
+  def error_messages
+    @match_proposal.errors.full_messages.to_sentence
+  end
+
+  private
+
+  def create_match_proposal
+    match_proposal = MatchProposal.new(match_proposal_params)
+
+    if match_proposal.save
+      match_request_volunteer_ids.each do |volunteer_id|
+        MatchRequest.create!(volunteer_id: volunteer_id, status: 'pending', match_proposal_id: match_proposal.id)
+      end
+    end
+
+    match_proposal
+  end
+
+  def match_request_volunteer_ids
+    @params.keys.
+      select { |param| param.include?('select_for_email_') }.
+      map { |param| param.split('_').last }
+  end
+
+  def match_proposal_params
+    @params.permit(:day, :start_time, :end_time, :client_id, :status).merge(client_id: @params[:match_proposal][:client_id], status: 'pending')
+  end
+end
+
+
 class MatchProposalsController < ApplicationController
   def create
-    @match_proposal = MatchProposal.new(match_proposal_params)
-
-    if @match_proposal.save
-      match_request_volunteer_ids.each do |volunteer_id|
-        MatchRequest.create!(volunteer_id: volunteer_id, status: 'pending', match_proposal_id: @match_proposal.id)
-      end
+    service = CreateMatchProposal.new(params)
+    if service.successful?
+      @match_proposal = service.match_proposal
       redirect_to @match_proposal
     else
-      flash.now[:error] = @match_proposal.errors.full_messages.to_sentence
+      flash.now[:error] = service.error_messages
       @day_options = Date::DAYNAMES.zip(Date::DAYNAMES.map(&:downcase))
-
       render 'matches/explorer'
     end
   end
 
   def show
     @match_proposal = MatchProposal.find(params[:id])
-  end
-
-
-  private
-
-  def match_request_volunteer_ids
-    params.keys.
-      select { |param| param.include?('select_for_email_') }.
-      map { |param| param.split('_').last }
-  end
-
-  def match_proposal_params
-    params.permit(:day, :start_time, :end_time, :client_id, :status).merge(client_id: params[:match_proposal][:client_id], status: 'pending')
   end
 end
