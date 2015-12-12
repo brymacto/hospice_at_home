63f61a76ae26a3059ce2186ba6409ccb084fb505
Bryan MacDiarmid <bryanm@nulogy.com>
2015-12-12 15:50:32 -0500

    Updates seeds file and schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,41 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-10.times do
+20.times do
   Volunteer.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
   Client.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
 end
 
 10.times do
+  Match.create(
+    client_id: Client.order("RANDOM()").first.id,
+    volunteer_id: Volunteer.order("RANDOM()").first.id,
+    day: %w(monday tuesday wednesday thursday friday).sample,
+    start_time: [9, 10, 11].sample,
+    end_time: [12, 13, 14].sample)
+end
+
+60.times do
   VolunteerAvailability.create(
     volunteer_id: Volunteer.order('RANDOM()').first.id,
     start_hour: [9, 10, 13].sample,
     end_hour: [14, 16, 17].sample,
-    day: %w(monday tuesday).sample)
+    day: %w(monday tuesday wednesday thursday friday saturday sunday).sample
+  )
 end
+
+5.times do
+  match_proposal = MatchProposal.create(
+    client_id: Client.order("RANDOM()").first.id,
+    day: %w(monday tuesday wednesday thursday friday).sample,
+    start_time: [9, 10, 11].sample,
+    status: 'pending',
+    end_time: [12, 13, 14].sample
+  )
+
+  3.times do
+    MatchRequest.create(volunteer_id: Volunteer.order('RANDOM()').first.id, status: 'pending', match_proposal_id: match_proposal.id)
+  end
+
+end
+
