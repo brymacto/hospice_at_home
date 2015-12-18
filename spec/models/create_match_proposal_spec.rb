a941058c24d34d367661d38b3f656b95f8151144
Bryan MacDiarmid <bryanm@nulogy.com>
2015-12-18 12:15:21 -0500

    Adds spec for invalid match proposal.

--- spec/models/create_match_proposal_spec.rb
+++ spec/models/create_match_proposal_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe CreateMatchProposal do
-  it 'creates a valid match proposal' do
+  it 'saves a valid match proposal' do
     params = ActionController::Parameters.new({day: 'monday', start_time: 9, end_time: 10, client_id: 1, status: 'pending'})
 
     expect {
@@ -9,5 +9,15 @@ describe CreateMatchProposal do
     }.to change {
       MatchProposal.count
     }.by(1)
+  end
+
+  it 'doesnt save a non-valid match proposal'  do
+    params = ActionController::Parameters.new({day: 'monday', start_time: nil, end_time: 10, client_id: 1, status: 'pending'})
+
+    expect {
+      CreateMatchProposal.new(params)
+    }.to change {
+      MatchProposal.count
+    }.by(0)
   end
 end
