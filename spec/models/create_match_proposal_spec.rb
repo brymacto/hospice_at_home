297e10d20f6dc8ef88bb92f6e0355349d66ee18b
Jason CheongKeeYou <jasonc@nulogy.com>
2015-12-18 12:29:21 -0500

    Improves test readability.

--- spec/models/create_match_proposal_spec.rb
+++ spec/models/create_match_proposal_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe CreateMatchProposal do
   it 'saves a valid match proposal' do
-    params = ActionController::Parameters.new({day: 'monday', start_time: 9, end_time: 10, client_id: 1, status: 'pending'})
+    params = build_params
 
     expect {
       CreateMatchProposal.new(params)
@@ -11,13 +11,17 @@ describe CreateMatchProposal do
     }.by(1)
   end
 
-  it 'doesnt save a non-valid match proposal'  do
-    params = ActionController::Parameters.new({day: 'monday', start_time: nil, end_time: 10, client_id: 1, status: 'pending'})
+  it 'does not save a invalid match proposal'  do
+    params = build_params.merge(start_time: nil)
 
     expect {
       CreateMatchProposal.new(params)
     }.to change {
       MatchProposal.count
     }.by(0)
+  end
+
+  def build_params
+    ActionController::Parameters.new({day: 'monday', start_time: 9, end_time: 10, client_id: 1, status: 'pending'})
   end
 end
