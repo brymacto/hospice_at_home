ea90867208c3d95bc899a64b0a2dc6ac0e47fea4
Bryan MacDiarmid <bryanm@nulogy.com>
2015-12-18 12:10:12 -0500

    Refactors spec.

--- spec/models/create_match_proposal_spec.rb
+++ spec/models/create_match_proposal_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 describe CreateMatchProposal do
-
   it 'creates a valid match proposal' do
     params = ActionController::Parameters.new({day: 'monday', start_time: 9, end_time: 10, client_id: 1, status: 'pending'})
-    initial_count = MatchProposal.count
-    service = CreateMatchProposal.new(params)
 
-    expect(MatchProposal.count - initial_count).to be(1)
+    expect {
+      CreateMatchProposal.new(params)
+    }.to change {
+      MatchProposal.count
+    }.by(1)
   end
 end
