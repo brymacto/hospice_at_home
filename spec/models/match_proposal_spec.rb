4a2f878372dba9930ac2e70ade1cd559fbfd10dd
Jason CheongKeeYou <jasonc@nulogy.com>
2015-12-18 12:42:25 -0500

    Adds test for MatchProposal validation.

--- spec/models/match_proposal_spec.rb
+++ spec/models/match_proposal_spec.rb
@@ -27,6 +27,12 @@ describe MatchProposal do
     expect(match_proposal).to_not be_valid
   end
 
+  it 'validates that end_time is number' do
+    match_proposal = MatchProposal.new(start_time: 0, end_time: 'a', day: 'foo')
+
+    expect(match_proposal).to_not be_valid
+  end
+
   it 'validates that end_time is between 1 and 24' do
     match_proposal = MatchProposal.new(start_time: 0, end_time: 0, day: 'foo')
     expect(match_proposal).to_not be_valid
@@ -38,6 +44,18 @@ describe MatchProposal do
     expect(match_proposal).to be_valid
 
     match_proposal = MatchProposal.new(start_time: 0, end_time: 25, day: 'foo')
+    expect(match_proposal).to_not be_valid
+  end
+
+  it 'validates day is present' do
+    match_proposal = MatchProposal.new(start_time: 0, end_time: 24, day: '')
+
+    expect(match_proposal).to_not be_valid
+  end
+
+  it 'validates that start_time precedes end_time' do
+    match_proposal = MatchProposal.new(start_time: 1, end_time: 1, day: 'foo')
+
     expect(match_proposal).to_not be_valid
   end
 end
