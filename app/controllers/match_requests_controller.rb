1e3d4ce18b814709a04188e9a28dd4ba87349144
Bryan MacDiarmid <bryanm@nulogy.com>
2015-12-24 14:23:26 -0500

    Creates match when match request accepted.

--- app/controllers/match_requests_controller.rb
+++ app/controllers/match_requests_controller.rb
@@ -3,16 +3,30 @@ class MatchRequestsController < ApplicationController
     load_match_request
     @match_request.update!(match_request_params)
     set_parent_proposal_to_accepted if @match_request.status == 'accepted'
+    create_match if first_request_accepted?
     redirect_to @match_request.match_proposal
   end
 
   private
+  def create_match
+    match_proposal = @match_request.match_proposal
+    fail 'A match already exists for this match proposal' if @match_request.match
 
-  def set_parent_proposal_to_accepted
-    @match_request.match_proposal.update!({ status: 'accepted' })
+    Match.create(
+      match_proposal_id: match_proposal.id,
+      client_id: match_proposal.client_id,
+      volunteer_id: @match_request.volunteer_id,
+      day: match_proposal.day,
+      start_time: match_proposal.start_time,
+      end_time: match_proposal.end_time
+    )
   end
 
-  def create_match_request?
+  def set_parent_proposal_to_accepted
+    @match_request.match_proposal.update!({status: 'accepted'})
+  end
+
+  def first_request_accepted?
     !@match_request.match && match_request_params[:status] == 'accepted'
   end
 
