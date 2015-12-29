d45e11c9fb6848e330ae7b89e60f6d17078e7ace
Bryan MacDiarmid <bryanm@nulogy.com>
2015-12-29 17:22:51 -0500

    Adds date formatting filter.

--- app/assets/javascripts/angular_app.js
+++ app/assets/javascripts/angular_app.js
@@ -14,9 +14,21 @@ angular.module('hospiceAtHome', ['ngResource'])
         $scope.matches = matchFactory.query();
       }])
     .filter('capitalize', function () {
-      return function (input, scope) {
+      return function (input) {
         if (input != null)
           input = input.toLowerCase();
         return input.substring(0, 1).toUpperCase() + input.substring(1);
       }
-    });
+    })
+    .filter('full_date', function() {
+      return function (input) {
+        full_date = "";
+        full_date += input['day'];
+        full_date += ' ';
+        full_date += input['start_time'];
+        full_date += ' to ';
+        full_date += input['end_time'];
+        return full_date;
+      }
+    })
+;
