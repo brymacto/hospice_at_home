ad47df79741a8d1eda1526e6716a202c52912a92
Bryan MacDiarmid <bryanm@nulogy.com>
2015-12-29 15:12:53 -0500

    Updates walking skeleton with match table.

--- app/assets/javascripts/angular_app.js
+++ app/assets/javascripts/angular_app.js
@@ -3,4 +3,41 @@ angular.module('hospiceAtHome', [])
       '$scope',
       function($scope){
         $scope.test = 'Hello world!';
+
+
+        $scope.matches = [
+          {
+            id: 16,
+            client_id: 7,
+            volunteer_id: 14,
+            created_at: "2015-12-24T20:08:18.086Z",
+            updated_at: "2015-12-24T20:08:18.086Z",
+            day: "monday",
+            start_time: 9,
+            end_time: 12,
+            match_request_id: 15
+          },
+          {
+            id: 16,
+            client_id: 7,
+            volunteer_id: 14,
+            created_at: "2015-12-24T20:08:18.086Z",
+            updated_at: "2015-12-24T20:08:18.086Z",
+            day: "monday",
+            start_time: 9,
+            end_time: 12,
+            match_request_id: 15
+          },
+          {
+            id: 16,
+            client_id: 7,
+            volunteer_id: 14,
+            created_at: "2015-12-24T20:08:18.086Z",
+            updated_at: "2015-12-24T20:08:18.086Z",
+            day: "monday",
+            start_time: 9,
+            end_time: 12,
+            match_request_id: 15
+          }
+        ];
       }]);
