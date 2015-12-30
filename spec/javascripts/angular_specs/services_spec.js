54aac0f8219e4a43678b9f81a37a491b214b247c
Bryan MacDiarmid <bryanm@nulogy.com>
2015-12-30 17:09:17 -0500

    Adds specs for ordering service

--- spec/javascripts/angular_specs/services_spec.js
+++ spec/javascripts/angular_specs/services_spec.js
@@ -4,29 +4,56 @@
 describe('service', function () {
 
   beforeEach(module('hospiceAtHome'));
+
   describe('orderingService', function () {
+
     describe('setOrderProp', function () {
       it('sets order property correctly when sorting client', inject(function (orderingService) {
-        orderBy = 'client';
+        var orderBy = 'client';
         result = orderingService.setOrderProp(orderBy);
         expect(result).toEqual(['client.last_name', 'client.first_name']);
       }));
       it('sets order property correctly when sorting volunteer', inject(function (orderingService) {
-        orderBy = 'volunteer';
+        var orderBy = 'volunteer';
         result = orderingService.setOrderProp(orderBy);
         expect(result).toEqual(['volunteer.last_name', 'volunteer.first_name']);
       }));
       it('sets order property correctly when sorting day and time', inject(function (orderingService) {
-        orderBy = 'date';
+        var orderBy = 'date';
         result = orderingService.setOrderProp(orderBy);
         expect(result).toEqual(['day_number', 'start_time']);
       }));
       it('sets order property correctly when sorting other value', inject(function (orderingService) {
-        orderBy = 'foo';
+        var orderBy = 'foo';
         result = orderingService.setOrderProp(orderBy);
         expect(result).toEqual('foo');
       }));
     });
 
+    describe('setOrderAscending', function () {
+      it('reverses orderAscending value when last call was for same column', inject(function (orderingService) {
+        var orderBy = 'client';
+        var orderAscending = true;
+        var orderByLast = 'client';
+
+
+        result = orderingService.setOrderAscending(orderBy, orderByLast, orderAscending);
+
+        expect(result).toBe(false)
+      }));
+
+      it('sets orderAscending value to false when last call was for different column', inject(function (orderingService) {
+        var orderBy = 'client';
+        var orderAscending = false;
+        var orderAscending_2 = true;
+        var orderByLast = 'volunteer';
+
+        result = orderingService.setOrderAscending(orderBy, orderByLast, orderAscending);
+        result_2 = orderingService.setOrderAscending(orderBy, orderByLast, orderAscending_2);
+
+        expect(result).toBe(false)
+        expect(result_2).toBe(false)
+      }));
+    });
   });
 });
