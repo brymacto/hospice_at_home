8114265ff60bb03b7a083978d88a23cf554f6272
Bryan MacDiarmid <bryanm@nulogy.com>
2015-12-30 17:59:00 -0500

    Adds angular controller spec for JSON.

--- spec/javascripts/angular_specs/controllers_spec.js
+++ spec/javascripts/angular_specs/controllers_spec.js
@@ -1,10 +1,112 @@
+//= require 'angular-resource'
+
 describe('MatchCtrl', function () {
+  var scope, ctrl, $httpBackend;
 
-  it('sets acsending order to false', inject(function ($controller) {
-    var scope = {}
-    var ctrl = $controller('MatchCtrl', {$scope: scope});
+  beforeEach(function () {
+    jasmine.addMatchers({
+      toEqualData: function () {
+        return {
+          compare: function (actual, expected) {
+            return {
+              pass: angular.equals(actual, expected)
+            };
+          }
+        };
+      }
+    });
+  });
 
-    expect(scope.orderAscending).toBe(false);
+  beforeEach(inject(function (_$httpBackend_, $rootScope, $controller) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('/matches.json').
+    respond(
+        [
+          {
+            id: 1,
+            day: "monday",
+            day_number: 1,
+            start_time: 9,
+            end_time: 12,
+            volunteer: {
+              id: 14,
+              last_name: "Dietrich",
+              first_name: "Anna"
+            },
+            client: {
+              id: 7,
+              last_name: "Adams",
+              first_name: "Golda"
+            }
+          },
+          {
+            id: 2,
+            day: "friday",
+            day_number: 5,
+            start_time: 9,
+            end_time: 13,
+            volunteer: {
+              id: 30,
+              last_name: "Lind",
+              first_name: "Brendon"
+            },
+            client: {
+              id: 27,
+              last_name: "Barrows",
+              first_name: "Arturo"
+            }
+          }
+        ]
+    );
+
+    scope = $rootScope.$new();
+    ctrl = $controller('MatchCtrl', {$scope: scope});
   }));
 
+  it('sets acsending order to false', function () {
+    expect(scope.orderAscending).toBe(false);
+  });
+
+  it('creates a matches model with two matches', function () {
+    expect(scope.matches).toEqualData([]);
+    $httpBackend.flush();
+    expect(scope.matches).toEqualData(
+        [
+          {
+            id: 1,
+            day: "monday",
+            day_number: 1,
+            start_time: 9,
+            end_time: 12,
+            volunteer: {
+              id: 14,
+              last_name: "Dietrich",
+              first_name: "Anna"
+            },
+            client: {
+              id: 7,
+              last_name: "Adams",
+              first_name: "Golda"
+            }
+          },
+          {
+            id: 2,
+            day: "friday",
+            day_number: 5,
+            start_time: 9,
+            end_time: 13,
+            volunteer: {
+              id: 30,
+              last_name: "Lind",
+              first_name: "Brendon"
+            },
+            client: {
+              id: 27,
+              last_name: "Barrows",
+              first_name: "Arturo"
+            }
+          }
+        ]
+    );
+  });
 });
