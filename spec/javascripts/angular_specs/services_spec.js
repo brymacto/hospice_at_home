6fffa175116a4de1a949ca7afeaf178a6c1c0e0f
Bryan MacDiarmid <bryanm@nulogy.com>
2015-12-30 17:13:39 -0500

    Refactors spec.

--- spec/javascripts/angular_specs/services_spec.js
+++ spec/javascripts/angular_specs/services_spec.js
@@ -7,42 +7,49 @@ describe('service', function () {
 
   describe('orderingService', function () {
 
+    beforeEach(inject(function (_orderingService_) {
+      orderingService = _orderingService_;
+    }));
+
     describe('setOrderProp', function () {
-      it('sets order property correctly when sorting client', inject(function (orderingService) {
+
+      it('sets order property correctly when sorting client', function () {
         var orderBy = 'client';
         result = orderingService.setOrderProp(orderBy);
         expect(result).toEqual(['client.last_name', 'client.first_name']);
-      }));
-      it('sets order property correctly when sorting volunteer', inject(function (orderingService) {
+      });
+
+      it('sets order property correctly when sorting volunteer', function () {
         var orderBy = 'volunteer';
         result = orderingService.setOrderProp(orderBy);
         expect(result).toEqual(['volunteer.last_name', 'volunteer.first_name']);
-      }));
-      it('sets order property correctly when sorting day and time', inject(function (orderingService) {
+      });
+
+      it('sets order property correctly when sorting day and time', function () {
         var orderBy = 'date';
         result = orderingService.setOrderProp(orderBy);
         expect(result).toEqual(['day_number', 'start_time']);
-      }));
-      it('sets order property correctly when sorting other value', inject(function (orderingService) {
+      });
+
+      it('sets order property correctly when sorting other value', function () {
         var orderBy = 'foo';
         result = orderingService.setOrderProp(orderBy);
         expect(result).toEqual('foo');
-      }));
+      });
     });
 
     describe('setOrderAscending', function () {
-      it('reverses orderAscending value when last call was for same column', inject(function (orderingService) {
+      it('reverses orderAscending value when last call was for same column', function () {
         var orderBy = 'client';
         var orderAscending = true;
         var orderByLast = 'client';
 
-
         result = orderingService.setOrderAscending(orderBy, orderByLast, orderAscending);
 
         expect(result).toBe(false)
-      }));
+      });
 
-      it('sets orderAscending value to false when last call was for different column', inject(function (orderingService) {
+      it('sets orderAscending value to false when last call was for different column', function () {
         var orderBy = 'client';
         var orderAscending = false;
         var orderAscending_2 = true;
@@ -53,7 +60,7 @@ describe('service', function () {
 
         expect(result).toBe(false)
         expect(result_2).toBe(false)
-      }));
+      });
     });
   });
 });
