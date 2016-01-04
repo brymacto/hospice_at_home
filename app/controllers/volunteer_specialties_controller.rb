afb306c561b17fd9de74fbc41bc33509832a1e38
Bryan MacDiarmid <bryanm@nulogy.com>
2016-01-04 13:45:47 -0500

    Allows editing of specialties.

--- app/controllers/volunteer_specialties_controller.rb
+++ app/controllers/volunteer_specialties_controller.rb
@@ -22,6 +22,20 @@ class VolunteerSpecialtiesController < ApplicationController
     @volunteers = @volunteer_specialty.volunteers
   end
 
+  def edit
+    load_volunteer_specialty
+  end
+
+  def update
+    load_volunteer_specialty
+    if @volunteer_specialty.update(volunteer_specialty_params)
+      redirect_to @volunteer_specialty
+    else
+      flash.now[:error] = @volunteer_specialty.errors.full_messages.to_sentence
+      render 'edit'
+    end
+  end
+
   private
 
   def load_volunteer_specialty
