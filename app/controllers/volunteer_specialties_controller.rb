b606decdf1f1d5811680d40dca061443df1c3a21
Bryan MacDiarmid <bryanm@nulogy.com>
2016-01-03 20:05:22 -0500

    Adds specialty views

--- app/controllers/volunteer_specialties_controller.rb
+++ app/controllers/volunteer_specialties_controller.rb
@@ -2,4 +2,33 @@ class VolunteerSpecialtiesController < ApplicationController
   def index
     @volunteer_specialties = VolunteerSpecialty.all
   end
+
+  def new
+    @volunteer_specialty = VolunteerSpecialty.new
+  end
+
+  def create
+    @volunteer_specialty = VolunteerSpecialty.new(volunteer_specialty_params)
+    if @volunteer_specialty.save
+      redirect_to @volunteer_specialty
+    else
+      flash.now[:error] = @volunteer_specialty.errors.full_messages.to_sentence
+      render 'new'
+    end
+  end
+
+  def show
+    load_volunteer_specialty
+    @volunteers = @volunteer_specialty.volunteers
+  end
+
+  private
+
+  def load_volunteer_specialty
+    @volunteer_specialty = VolunteerSpecialty.find(params[:id])
+  end
+
+  def volunteer_specialty_params
+    params.require(:volunteer_specialty).permit(:name)
+  end
 end
