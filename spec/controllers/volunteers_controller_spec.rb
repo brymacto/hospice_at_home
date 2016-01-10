73210bd906f0c4727818d4b5c42e98ebe9b88ae2
Bryan MacDiarmid <bryanm@nulogy.com>
2016-01-09 21:58:54 -0500

    Adds volunteer controller specs.

--- spec/controllers/volunteers_controller_spec.rb
+++ spec/controllers/volunteers_controller_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe VolunteersController, type: :controller do
+
   describe 'volunteer availability POST' do
+
+    let(:test_volunteer) do
+      create(:volunteer)
+    end
+
     it 'redirects user to volunteer edit page and displays flash message when there are validation errors' do
-      # availability = VolunteerAvailability.new(start_hour: 9, end_hour: 10, day: nil)
-
-      test_volunteer = create(:volunteer)
-
       post(:add_volunteer_availabilities,
            id: test_volunteer.id,
            volunteer_availability: { start_hour: 9, end_hour: 10, day: '' }
@@ -14,5 +16,66 @@ RSpec.describe VolunteersController, type: :controller do
 
       expect(flash[:error]).to eq("Day can't be blank")
     end
+  end
+
+  describe '#add_volunteer_availabilities' do
+    let(:test_volunteer) do
+      create(:volunteer)
+    end
+
+    it 'does not add invalid availability' do
+      get :add_volunteer_availabilities, id: test_volunteer.id, volunteer_availability: { start_hour: '9', end_hour: '8', day: 'monday', volunteer_id: test_volunteer.id}
+      expect(assigns[:volunteer_availabilities].size).to eq(0)
+      expect(flash[:error]).to eq("Start hour must be before end hour")
+    end
+
+    it 'adds valid availability' do
+      post :add_volunteer_availabilities, id: test_volunteer.id, volunteer_availability: { start_hour: '9', end_hour: '10', day: 'monday', volunteer_id: test_volunteer.id}
+      expect(assigns[:volunteer_availabilities].size).to eq(1)
+    end
+
+    it 'redirects to volunteer show view' do
+      get :add_volunteer_availabilities, id: test_volunteer.id, volunteer_availability: { start_hour: '9', end_hour: '10', day: 'monday', volunteer_id: test_volunteer.id}
+      expect(response).to redirect_to(volunteer_path(test_volunteer))
+    end
+  end
+
+  describe '#add_volunteer_specialty' do
+    let(:test_volunteer) do
+      create(:volunteer)
+    end
+
+    let(:test_specialty) do
+      create(:volunteer_specialty)
+    end
+
+    it 'adds a volunteer specialty' do
+      expect(test_volunteer.volunteer_specialties.count).to eq(0)
+      patch :add_volunteer_specialty, id: test_volunteer.id, volunteer: {volunteer_specialty_ids: test_specialty.id}
+      expect(test_volunteer.volunteer_specialties.count).to eq(1)
+    end
+
+  end
+
+  describe '#remove_volunteer_specialty' do
+    let(:test_specialty) do
+      create(:volunteer_specialty)
+    end
+
+    let(:test_volunteer) do
+      create(:volunteer, volunteer_specialties: [test_specialty])
+    end
+
+    it 'removes volunteer specialty' do
+      expect(test_volunteer.volunteer_specialties.count).to eq(1)
+      get :remove_volunteer_specialty, id: test_volunteer.id, volunteer_specialty_id: test_specialty.id
+      expect(test_volunteer.volunteer_specialties.count).to eq(0)
+    end
+
+    it 'redirects to volunteer show view' do
+      get :remove_volunteer_specialty, id: test_volunteer.id, volunteer_specialty_id: test_specialty.id
+      expect(response).to redirect_to(volunteer_path(test_volunteer))
+    end
+
   end
 end
