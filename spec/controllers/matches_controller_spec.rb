ba552dd7e887f6832737af1a014e648dd1ada730
Bryan MacDiarmid <bryanm@nulogy.com>
2016-01-09 20:42:36 -0500

    Adds spec.

--- spec/controllers/matches_controller_spec.rb
+++ spec/controllers/matches_controller_spec.rb
@@ -19,12 +19,14 @@ RSpec.describe MatchesController, type: :controller do
       last_name: Faker::Name.last_name,
       volunteer_specialties: [test_volunteer_specialty])
   end
+
   let!(:test_volunteer_not_available) do
     create(
       :volunteer,
       first_name: Faker::Name.first_name,
       last_name: Faker::Name.last_name)
   end
+
   let!(:test_volunteer_availability) do
     create(:volunteer_availability,
            volunteer_id: test_volunteer_available.id,
@@ -58,8 +60,14 @@ RSpec.describe MatchesController, type: :controller do
       expect(assigns(:volunteers)).to_not include(test_volunteer_not_available)
     end
 
-    it 'assigns empty @volunteers when none match search params' do
+    it 'assigns empty @volunteers when none match search params based on availability' do
       get :explorer, match_exploration: { day: 'tuesday', start_time: 10, end_time: 12, client_id: 1 }
+
+      expect(assigns(:volunteers)).to be_empty
+    end
+
+    it 'assigns empty @volunteers when none match search params based on specialty' do
+      get :explorer, match_exploration: { day: 'monday', start_time: 10, end_time: 12, client_id: 1, specialty_id: test_volunteer_specialty.id + 1 }
 
       expect(assigns(:volunteers)).to be_empty
     end
