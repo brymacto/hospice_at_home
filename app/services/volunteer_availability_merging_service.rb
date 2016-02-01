f659eefa5ef44880cf11a530c32587a85ba3e1db
Bryan MacDiarmid <bryanm@nulogy.com>
2016-01-31 21:55:37 -0500

    Refactors production code.

--- app/services/volunteer_availability_merging_service.rb
+++ app/services/volunteer_availability_merging_service.rb
@@ -9,7 +9,9 @@ class VolunteerAvailabilityMergingService
   def merge_volunteer_availability
     @availabilities.each do |availability|
       return if @availabilities_already_merged.include?(availability)
+
       bordering_availabilities = get_bordering_availabilities(availability)
+
       bordering_availabilities.each do |bordering_availability|
         merge_availabilities(availability, bordering_availability)
       end
@@ -20,28 +22,16 @@ class VolunteerAvailabilityMergingService
 
   def merge_availabilities(availability_1, availability_2)
     ordered_availabilities = get_ordered_availabilities(availability_1, availability_2)
-    day = availability_1.day
 
-    create_availability(
-      day,
-      ordered_availabilities[0].start_hour,
-      ordered_availabilities[1].end_hour
-    )
+    create_merged_availability(availability_1.day, ordered_availabilities[0], ordered_availabilities[1])
 
     mark_availabilities_as_merged(availability_1, availability_2)
+
     destroy_availabilities(availability_1, availability_2)
   end
 
   def mark_availabilities_as_merged(*availabilities)
     availabilities.each { |availability| @availabilities_already_merged << availability }
-  end
-
-  def destroy_availabilities(*availabilities)
-    availabilities.each(&:destroy)
-  end
-
-  def create_availability(day, start_hour, end_hour)
-    VolunteerAvailability.create!(day: day, start_hour: start_hour, end_hour: end_hour, volunteer: @volunteer)
   end
 
   def get_ordered_availabilities(availability_1, availability_2)
@@ -51,20 +41,24 @@ class VolunteerAvailabilityMergingService
   end
 
   def get_bordering_availabilities(availability)
-    bordering_availabilities = []
-
-    @availabilities.each do |availability_for_comparison|
-      if availabilities_are_bordering(availability, availability_for_comparison)
-        bordering_availabilities << availability_for_comparison
-      end
+    @availabilities.select do |availability_for_comparison|
+      availabilities_are_bordering(availability, availability_for_comparison)
     end
-    bordering_availabilities
   end
 
   def availabilities_are_bordering(availability_1, availability_2)
     return false if availability_1 == availability_2
-    return true if (availability_1.day == availability_2.day) && (availability_1.start_hour == availability_2.end_hour || availability_1.end_hour == availability_2.start_hour)
+    return true if (availability_1.day == availability_2.day) &&
+      (availability_1.start_hour == availability_2.end_hour ||
+        availability_1.end_hour == availability_2.start_hour)
     false
   end
 
+  def destroy_availabilities(*availabilities)
+    availabilities.each(&:destroy)
+  end
+
+  def create_merged_availability(day, early_availability, late_availability)
+    VolunteerAvailability.create!(day: day, start_hour: early_availability.start_hour, end_hour: late_availability.end_hour, volunteer: @volunteer)
+  end
 end
