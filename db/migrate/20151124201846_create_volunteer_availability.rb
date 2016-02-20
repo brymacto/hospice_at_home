ae7c1fa5ea3cea00e432f85aec4b930d2037f452
Bryan MacDiarmid <bryanm@nulogy.com>
2016-02-20 16:30:01 -0500

    Fixes migrations that were edited previously by RubyMine renaming, unintentionally

--- db/migrate/20151124201846_create_volunteer_availability.rb
+++ db/migrate/20151124201846_create_volunteer_availability.rb
@@ -2,8 +2,8 @@ class CreateVolunteerAvailability < ActiveRecord::Migration
   def change
     create_table :volunteer_availabilities do |t|
       t.string :day, null: false
-      t.integer :start_time, null: false
-      t.integer :end_time, null: false
+      t.integer :start_hour, null: false
+      t.integer :end_hour, null: false
       t.integer :volunteer_id, null: false
 
       t.timestamps null: false
--- db/migrate/20160208140445_rename_hour_to_time.rb
+++ db/migrate/20160208140445_rename_hour_to_time.rb
@@ -1,6 +1,6 @@
 class RenameHourToTime < ActiveRecord::Migration
   def change
-    rename_column :availabilities, :start_time, :start_time
-    rename_column :availabilities, :end_time, :end_time
+    rename_column :availabilities, :start_hour, :start_time
+    rename_column :availabilities, :end_hour, :end_time
   end
 end
