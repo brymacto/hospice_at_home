7b5530f7d98c38e011cea2cc04cf5564b2d86578
Bryan MacDiarmid <bryanm@nulogy.com>
2016-03-05 10:53:49 -0500

    Refactors breadcrumb generator (again)

--- app/models/breadcrumb_generator.rb
+++ app/models/breadcrumb_generator.rb
@@ -1,50 +1,53 @@
 module BreadcrumbGenerator
   def load_breadcrumbs(crumb_class: nil, crumb_instance: nil, crumb_actions: [])
-    validate_input(crumb_class, crumb_instance, crumb_actions)
-
+    @crumb_class = crumb_class
+    @crumb_instance = crumb_instance
+    @crumb_actions = crumb_actions
     @breadcrumb_links = []
 
-    load_class_breadcrumb(crumb_class)
+    validate_input
 
-    load_instance_breadcrumb(crumb_instance)
+    load_class_breadcrumb
 
-    load_action_breadcrumbs(crumb_class, crumb_instance, crumb_actions)
+    load_instance_breadcrumb
+
+    load_action_breadcrumbs
 
     @breadcrumb_links
   end
 
   private
 
-  def validate_input(_crumb_class, _crumb_instance, crumb_actions)
-    fail 'Crumb actions must be provided as an Array' if crumb_actions.class != Array
+  def validate_input
+    fail 'Crumb actions must be provided as an Array' if @crumb_actions && @crumb_actions.class != Array
   end
 
-  def load_class_breadcrumb(crumb_class)
-    @breadcrumb_links.push(class_breadcrumb(crumb_class)) if crumb_class
+  def load_class_breadcrumb
+    @breadcrumb_links.push(class_breadcrumb) if @crumb_class
   end
 
-  def load_instance_breadcrumb(crumb_instance)
-    @breadcrumb_links.push(instance_breadcrumb(crumb_instance)) if crumb_instance
+  def load_instance_breadcrumb
+    @breadcrumb_links.push(instance_breadcrumb) if @crumb_instance
   end
 
-  def load_action_breadcrumbs(crumb_class, crumb_instance, crumb_actions)
-    crumb_actions.each do |crumb_action|
-      @breadcrumb_links.push(action_breadcrumb(crumb_class, crumb_instance, crumb_action))
+  def load_action_breadcrumbs
+    @crumb_actions.each do |crumb_action|
+      @breadcrumb_links.push(action_breadcrumb(crumb_action))
     end
   end
 
-  def class_breadcrumb(breadcrumb_class)
-    { path: url_for(breadcrumb_class), name: breadcrumb_class_name(breadcrumb_class) }
+  def class_breadcrumb
+    { path: url_for(@crumb_class), name: breadcrumb_class_name(@crumb_class) }
   end
 
-  def instance_breadcrumb(breadcrumb_instance)
-    { path: url_for(breadcrumb_instance), name: breadcrumb_instance.name }
+  def instance_breadcrumb
+    { path: url_for(@crumb_instance), name: @crumb_instance.name }
   end
 
-  def action_breadcrumb(breadcrumb_class, breadcrumb_instance, action)
+  def action_breadcrumb(action)
     return match_explorer_breadcrumb if action == :match_explorer
-    breadcrumb_instance_id = (breadcrumb_instance ? breadcrumb_instance.id : nil)
-    { path: url_for(controller: breadcrumb_controller_name(breadcrumb_class), action: action, id: breadcrumb_instance_id), name: action.to_s.capitalize }
+    breadcrumb_instance_id = (@crumb_instance ? @crumb_instance.id : nil)
+    { path: url_for(controller: breadcrumb_controller_name(@crumb_class), action: action, id: breadcrumb_instance_id), name: action.to_s.capitalize }
   end
 
   def match_explorer_breadcrumb
