5cbd7007a7290954f5b1d39040e45c326fc7c9cc
Bryan MacDiarmid <bryanm@nulogy.com>
2016-03-20 22:36:56 -0400

    Refactors to use Time Range value object for availability comparison

--- app/models/time_range.rb
+++ app/models/time_range.rb
@@ -14,4 +14,34 @@ class TimeRange
   def contains(other_time_range)
     day == other_time_range.day && start_time <= other_time_range.start_time && end_time >= other_time_range.end_time
   end
+
+  def has_same_day(other_time_range)
+    day == other_time_range.day
+  end
+
+  def borders(other_time_range)
+    has_same_day(other_time_range) &&
+      (start_time == other_time_range.end_time ||
+        end_time == other_time_range.start_time)
+  end
+
+  def overlaps(other_time_range)
+    has_same_day(other_time_range) && (partially_overlaps(other_time_range) || fully_overlaps(other_time_range))
+  end
+
+  def overlaps_or_is_overlapped_by(other_time_range)
+    overlaps(other_time_range) || other_time_range.overlaps(self)
+  end
+
+  private
+
+  def partially_overlaps(other_time_range)
+    ((@start_time <= other_time_range.start_time &&
+      @end_time >= other_time_range.start_time &&
+      @end_time <= other_time_range.end_time))
+  end
+
+  def fully_overlaps(other_time_range)
+    (@start_time >= other_time_range.start_time && @end_time <= other_time_range.end_time)
+  end
 end
--- app/services/availability_comparison_service.rb
+++ app/services/availability_comparison_service.rb
@@ -5,53 +5,23 @@ class AvailabilityComparisonService
   end
 
   def availabilities_are_duplicates
-    !availabilities_are_same_object &&
-      availabilities_have_same_day &&
-      @availability_1.start_time == @availability_2.start_time &&
-      @availability_1.end_time == @availability_2.end_time &&
-      @availability_1.volunteer == @availability_2.volunteer
+    return false if availabilities_are_same_object
+    @availability_1.availability_time == @availability_2.availability_time
   end
 
   def availabilities_are_bordering
-    !availabilities_are_same_object &&
-      (availabilities_have_same_day) &&
-      (@availability_1.start_time == @availability_2.end_time ||
-        @availability_1.end_time == @availability_2.start_time)
+    return false if availabilities_are_same_object
+    @availability_1.availability_time.borders(@availability_2.availability_time)
   end
 
   def availabilities_are_overlapping
-    !availabilities_are_same_object &&
-      availabilities_have_same_day &&
-      (availabilities_are_fully_overlapping || availabilities_are_partially_overlapping)
+    return false if availabilities_are_same_object
+    @availability_1.availability_time.overlaps_or_is_overlapped_by(@availability_2.availability_time)
   end
 
   private
 
-  def availabilities_are_fully_overlapping
-    overlaps_fully(@availability_1, @availability_2) ||
-      overlaps_fully(@availability_2, @availability_1)
-  end
-
-  def availabilities_are_partially_overlapping
-    overlaps_partially(@availability_1, @availability_2) ||
-      overlaps_partially(@availability_2, @availability_1)
-  end
-
-  def overlaps_fully(availability_1, availability_2)
-    (availability_1.start_time >= availability_2.start_time && availability_1.end_time <= availability_2.end_time)
-  end
-
-  def overlaps_partially(availability_1, availability_2)
-    ((availability_1.start_time <= availability_2.start_time &&
-      availability_1.end_time >= availability_2.start_time &&
-      availability_1.end_time <= availability_2.end_time))
-  end
-
   def availabilities_are_same_object
     @availability_1 == @availability_2
-  end
-
-  def availabilities_have_same_day
-    @availability_1.day == @availability_2.day
   end
 end
